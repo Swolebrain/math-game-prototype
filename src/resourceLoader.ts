9cc72d999c4c8c42b0ef15a0905255b7204ca613
swolebrain <thecodingteacher@gmail.com>
2024-01-11 20:44:33 -0500

    more log statements

--- src/resourceLoader.ts
+++ src/resourceLoader.ts
@@ -3,9 +3,12 @@ import { CharacterAnimation } from "./basetypes";
 export async function loadImagesIntoSpriteMap(spriteFiles: Record<CharacterAnimation, string[]>, spritesMap: Record<CharacterAnimation, HTMLImageElement[]>, animationTicksPerFrame: number) {
     const loader = Object.entries(spriteFiles).map(([animationType, animationFileNames]) => {
         return animationFileNames.map((fileName) => {
+            console.log(`Building img promise for ${fileName}`);
             return new Promise<[CharacterAnimation, HTMLImageElement]>((resolve, reject) => {
                 const image = new Image();
+                console.log(`Built image for ${fileName}`);
                 image.addEventListener('load', () => {
+                    console.log(`image load event listener for ${fileName}`);
                     resolve([animationType as CharacterAnimation, image]);
                 });
                 image.addEventListener('error', (e) => {
@@ -32,9 +35,12 @@ export async function loadImagesIntoSpriteMap(spriteFiles: Record<CharacterAnima
 
 export async function loadAudioFilesIntoAudioElements(audioFiles: Partial<Record<CharacterAnimation, string>>, audioMap: Partial<Record<CharacterAnimation, HTMLAudioElement>>) {
     const loader = Object.entries(audioFiles).map(([animationType, audioFileName]) => {
+        console.log(`Building audio promise for ${audioFileName}`);
         return new Promise<[CharacterAnimation, HTMLAudioElement]>((resolve, reject) => {
             const audio = new Audio(audioFileName);
+            console.log(`Built audio file for ${audioFileName}`);
             const canPlayHandler = () => {
+                console.log(`canPlay event listener for ${audioFileName}`);
                 resolve([animationType as CharacterAnimation, audio]);
                 audio.removeEventListener('canplay', canPlayHandler);
             };
